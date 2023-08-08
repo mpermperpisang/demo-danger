f45a17c0241dc46c776fd6da0208f27dbd456c6e
Ferawati Hartanti Pratiwi <mpermperpisang@gmail.com>
2023-08-08 15:54:14 +0700

    fix: VERSI BENAR

--- dangerfile.js
+++ dangerfile.js
@@ -9,15 +9,15 @@ async function a() {
 
   const a = await danger.github.api.teams.listMembersInOrg(payloadB).then(result => result.data);
 
-  a.forEach((response) => {
-    const b = danger.github.api.users.getByUsername({
-      username: response.login
+  for (i = 0; i < a.length; ++i) {
+    const b = await danger.github.api.users.getByUsername({
+      username: a[i].login
     }).then(result => result.data.name)
 
     if (!d.includes(b) && b !== null) {
-      d.push({user: response.login, fullname: b})
+      d.push({user: a[i].login, fullname: b})
     }
-  });
+  }
 
   console.log(d)
 }
