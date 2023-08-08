777c2c8af2d8a289557a7d084a1026822a20d910
Ferawati Hartanti Pratiwi <mpermperpisang@gmail.com>
2023-08-08 15:49:07 +0700

    test: abc

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,25 @@
 const { danger } = require("danger");
 
-console.warn(`sample: ${danger.github.pr.title}`);
+async function a() {
+  const d = [];
+  const payloadB = {
+    org: 'pt-kompas-media-nusantara',
+    team_slug: 'quality-engineer',
+  }
 
-message(danger.github.pr.title);
-warn(danger.github.pr.title);
-fail(danger.github.pr.title);
+  const a = await danger.github.api.teams.listMembersInOrg(payloadB).then(result => result.data);
+
+  a.forEach((response) => {
+    const b = danger.github.api.users.getByUsername({
+      username: response.login
+    }).then(result => result.data.name)
+
+    if (!d.includes(b) && b !== null) {
+      d.push({user: response.login, fullname: b})
+    }
+  });
+
+  console.log(d)
+}
+
+a()
