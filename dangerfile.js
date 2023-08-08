d80acd70adc4620f1c6e8fffeaa21385fce59da2
Ferawati Hartanti Pratiwi <mpermperpisang@gmail.com>
2023-08-08 17:50:21 +0700

    fix: code format

--- dangerfile.js
+++ dangerfile.js
@@ -1,25 +1,28 @@
 const { danger } = require("danger");
 
 async function a() {
-  const d = [];
-  const payloadB = {
+  const d = {};
+  const payloadB = (slug) => ({
     org: 'pt-kompas-media-nusantara',
-    team_slug: 'quality-engineer',
-  }
+    team_slug: slug,
+  })
+  const payloadC = (username) => ({
+    username
+  })
 
-  const a = await danger.github.api.teams.listMembersInOrg(payloadB).then(result => result.data);
+  const a = await danger.github.api.teams.listMembersInOrg(payloadB('quality-engineer')).then(result => result.data);
 
   for (i = 0; i < a.length; ++i) {
-    const b = await danger.github.api.users.getByUsername({
-      username: a[i].login
-    }).then(result => result.data.name)
+    const b = await danger.github.api.users.getByUsername(
+      payloadC(a[i].login)
+    ).then(result => result.data.name)
 
-    if (!d.includes(b) && b !== null) {
-      d.push({user: a[i].login, fullname: b})
+    if ((b !== null) && (a[i].login !== 'qeomid-ide')) {
+      d[a[i].login] = b;
     }
   }
 
-  console.log(d)
+  console.log(d.mpermperpisang)
 }
 
 a()
